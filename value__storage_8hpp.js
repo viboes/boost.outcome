6907085a647e899e5c61e3f305bc2b5b4c76f698
Jenkins nedprod CI <foo@nowhere>
2017-01-21 16:20:29 +0000

    Travis CI updates documentation

--- value__storage_8hpp.js
+++ value__storage_8hpp.js
@@ -10,7 +10,6 @@ var value__storage_8hpp =
     [ "BOOST_OUTCOME_VALUE_STORAGE_NON_TRIVIAL_DESTRUCTOR", "value__storage_8hpp.html#a4352b627488d0b1906b138421bccaf7d", null ],
     [ "compare_if", "value__storage_8hpp.html#a2621637dae6890d7227bac59c67f4a37", null ],
     [ "exception_t", "value__storage_8hpp.html#ga93fd76af06145e1e6544d9c27bd080d0", null ],
-    [ "move_construct_if", "value__storage_8hpp.html#aaff7e458b6026a088b44a561cf1b820f", null ],
     [ "operator<<", "value__storage_8hpp.html#gab6f91d60f7f8d66cc60aec27fd61166a", null ],
     [ "operator<<", "value__storage_8hpp.html#a414adb7e415fad8bdfb9725db89842e2", null ],
     [ "operator<<", "value__storage_8hpp.html#ab0651b54f6479a27a1c8a305de326dfa", null ],
